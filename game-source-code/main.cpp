2d3de025f21c1810dd30eadf6ae0a67c11d80385
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-09-03 22:35:02 +0200

    Added main and test files-set gitignore to exclude doctest.h

---
+++ game-source-code/main.cpp
@@ -0,0 +1,10 @@
+// This code is adapted from: https://github.com/educ8s/Cpp-Pong-Game-Raylib
+
+#include <raylib-cpp.hpp>
+#include <iostream>
+using namespace raylib; // import the raylib-cpp namespace into the global namespace
+
+int main()
+{
+       return 0;
+}
---
+++ test-source-code/tests.cpp
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <raylib-cpp.hpp>
+#include <iostream>
+
+TEST_CASE("")
+{
+}
