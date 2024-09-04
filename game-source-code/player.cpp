e193dfba6695c3d6aeead6c9e53c29eeb53c6036
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-09-04 23:04:48 +0200

    Partial implementation of player movement function

--- game-source-code/player.cpp
+++ game-source-code/player.cpp
@@ -1,3 +1,30 @@
 #include "player.h"
 
-Player::Player(){}
+Player::Player():
+xPosition{800.0f},
+yPosition{450.0f}
+{}
+
+void Player::movePlayer(bool rightArrowKeyPressed, bool leftArrowKeyPressed,
+                        bool downArrowKeyPressed, bool upArrowKeyPressed)
+{
+    if (rightArrowKeyPressed)
+    {
+        //move player to the right 
+    }
+
+    if(leftArrowKeyPressed)
+    {
+        //Move player to the left
+    }
+
+    if(downArrowKeyPressed)
+    {
+        //move player downwards
+    }
+
+    if (upArrowKeyPressed)
+    {
+        //move player upwards
+    }
+}
--- game-source-code/player.h
+++ game-source-code/player.h
@@ -4,9 +4,12 @@
 class Player
 {
 private:
+ float xPosition;
+ float yPosition;
 
 public:
     Player();
+    void movePlayer(bool rightArrowKeyPressed, bool leftArrowKeyPressed, bool downArrowKeyPressed, bool upArrowKeyPressed);
 };
 #endif //PLAYER
 
