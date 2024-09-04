3050c15afd70c71f279107ca2b24a842ae98b841
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-09-04 23:22:20 +0200

    Added logic to restrict player within screen

--- game-source-code/player.cpp
+++ game-source-code/player.cpp
@@ -12,23 +12,33 @@ void Player::movePlayer(bool rightArrowKeyPressed, bool leftArrowKeyPressed,
     {
         //move player to the right
         xPosition += 2.5f;
+        if (xPosition >= 1426.0f)
+           xPosition = 1426.0f;
     }
 
     if(leftArrowKeyPressed)
     {
         //Move player to the left
         xPosition -= 2.5f;
+        if (xPosition <= 174.0f)
+            xPosition = 174.0f;
     }
 
     if(downArrowKeyPressed)
     {
         //move player downwards
         yPosition += 2.5f;
+
+        if (yPosition >= 711.5f)
+            yPosition = 711.5f;
     }
 
     if (upArrowKeyPressed)
     {
         //move player upwards
         yPosition -= 2.5f;
+        //Restrict player within bounds
+        if (yPosition <= 188.5f)
+            yPosition = 188.5f;
     }
 }
