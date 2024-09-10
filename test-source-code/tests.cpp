95ad5b10ef9cc7f566128cb2dea6a2cff67ae820
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-09-11 01:02:04 +0200

    Player test: Player Bounded upper bound of window

--- test-source-code/tests.cpp
+++ test-source-code/tests.cpp
@@ -149,3 +149,29 @@ TEST_CASE("TEST IF PLAYER IS BOUNDED ON LOWER BOUND OF SCREEN")
     CHECK_EQ(expectedXpos,actualXpos);
     CHECK_EQ(expectedYpos,actualYpos);
 }
+
+TEST_CASE("TEST IF PLAYER IS BOUNDED ON UPPER BOUND OF SCREEN")
+{
+    auto player = std::make_unique<Player>();
+    auto game_world = std::make_unique<GameWorld>();
+    game_world->loadMapFromFile();
+    std::vector<std::shared_ptr<GameWorldResources>>game_resources;
+    auto [InitXpos, InitYpos] = player->getPlayerPosition();
+    auto window_height = 704;
+    auto player_dimension = 32;
+    auto wall_width = 8.0f;
+    auto lower_boundary = wall_width;
+
+    bool movingLeft = false,movingRight = false,movingUp = true, movingDown = false;
+    for (int i = 0; i < 500; i++)
+    {
+        //Simulate the player attempting to move out of bounds
+        player->movePlayer(movingRight,movingLeft,movingDown,movingUp, game_resources);
+    }
+    
+    auto expectedYpos = lower_boundary;
+    auto expectedXpos = InitXpos;
+    auto [actualXpos, actualYpos] = player->getPlayerPosition();
+    CHECK_EQ(expectedXpos,actualXpos);
+    CHECK_EQ(expectedYpos,actualYpos);
+}
