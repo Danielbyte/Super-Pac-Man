46007c1b34ab360947d6ce74f948f5c4a13503da
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-09-14 16:38:07 +0200

    Stop watch function definitions

--- game-source-code/stopwatch.cpp
+++ game-source-code/stopwatch.cpp
@@ -1,3 +1,27 @@
 #include "stopwatch.h"
 
 StopWatch::StopWatch(){}
+
+std::chrono::time_point<std::chrono::high_resolution_clock> StopWatch::currentTime()
+{
+    auto time = std::chrono::high_resolution_clock::now();
+    return time;
+}
+
+float StopWatch::elapsedTime()
+{
+    stopTimer();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(finalTime-initialTime);
+	auto TimePassed = static_cast<float> (elapsedTime.count())/1000000.0f;
+	return TimePassed;
+}
+
+void StopWatch::stopTimer()
+{
+
+}
+
+void StopWatch::restartTimer()
+{
+
+}
--- game-source-code/stopwatch.h
+++ game-source-code/stopwatch.h
@@ -7,5 +7,14 @@ class StopWatch
 {
     public:
         StopWatch();
+        float elapsedTime();
+        void stopTimer();
+        void restartTimer();
+    
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> currentTime();
+        std::chrono::time_point<std::chrono::high_resolution_clock> initialTime;
+        std::chrono::time_point<std::chrono::high_resolution_clock> finalTime;
+
 };
 #endif
