dbd36ba2dfc1d3240b9dce6dc8031750708aa1b3
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-09-17 22:40:20 +0200

    Fruit class implementation

--- game-source-code/fruit.cpp
+++ game-source-code/fruit.cpp
@@ -1,22 +1,27 @@
 #include "fruit.h"
-Fruit::Fruit(){}
+Fruit::Fruit():
+xPosition{-100.0f},
+yPosition{-100.0f},//Initially, at position that is off the screen
+canDelete{false}
+{}
 
 void Fruit::setPosition(const float xPos,const float yPos)
 {
-
+    xPosition = xPos;
+    yPosition = yPos;
 }
 
 void Fruit::markForDeletion()
 {
-
+    canDelete = true;
 }
 
 bool Fruit::getIfCanDelete() const
 {
-
+    return canDelete;
 }
    
 std::tuple<float, float> Fruit::getPosition() const
 {
-    
+    return {xPosition, yPosition};
 }
