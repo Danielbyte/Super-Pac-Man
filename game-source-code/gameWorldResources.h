837cf3a86301ae0042e0b76ac3961c1fb7177442
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-10-05 10:26:26 +0200

    Removed unused variables

--- game-source-code/gameWorldResources.h
+++ game-source-code/gameWorldResources.h
@@ -28,13 +28,5 @@ float tileScreenPosY;
 std::shared_ptr<GameWorld>game_world = std::make_shared<GameWorld>();
 ObjectType type;
 
-raylib::Texture2D horizontalWallPieceT;
-raylib::Image horizontalWallPieceI;
-raylib::Texture2D verticalWallPieceT;
-raylib::Image verticalWallPieceI;
-
-std::vector<std::shared_ptr<GameWorldResources>>maze_resources = {};
-std::vector<std::shared_ptr<raylib::Texture2D>>game_world_textures = {};
-
 };
 #endif
