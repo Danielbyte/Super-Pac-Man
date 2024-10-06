a70024e4b9778e6fe94f24be1d29f3bf0ff55e37
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-10-06 13:06:10 +0200

    Load player texture into vectors

--- game-source-code/playerResources.cpp
+++ game-source-code/playerResources.cpp
@@ -1,3 +1,45 @@
 #include "playerResources.h"
 
 PlayerResources::PlayerResources(){}
+
+void PlayerResources::updateTexture(std::shared_ptr<Player>& player, std::shared_ptr<raylib::Texture2D>& player_texture,const bool buttoPressed)
+{
+    auto direction = player->getPlayerDirection();
+}
+
+void PlayerResources::initialiseTexture()
+{
+    //NORMAL MODE
+    auto pacmanRight1_T = std::make_shared<raylib::Texture2D>();
+    pacmanRight1_T->Load("../resources/pacmanRight1.png");
+    rightTexturesNM.push_back(pacmanRight1_T);
+
+    //Left
+    auto pacmanLeft1_T = std::make_shared<raylib::Texture2D>();
+    pacmanLeft1_T->Load("../resources/pacmanLeft1.png");
+
+    //Down
+    auto pacmanDown1_T = std::make_shared<raylib::Texture2D>();
+    pacmanDown1_T->Load("../resources/pacmanDown1.png");
+
+    //Up
+    auto pacmanUp1_T = std::make_shared<raylib::Texture2D>();
+    pacmanUp1_T->Load("../resources/pacmanUp1.png");
+
+     //SUPER-PACMAN MODE
+    //To the right
+    auto SuperpacmanRight1_T = std::make_shared<raylib::Texture2D>();
+    SuperpacmanRight1_T->Load("../resources/SuperpacmanRight1.png");
+
+    //To the left
+    auto SuperpacmanLeft1_T = std::make_shared<raylib::Texture2D>();
+    SuperpacmanLeft1_T->Load("../resources/SuperpacmanLeft1.png");
+
+    //Moving down
+    auto SuperpacmanDown1_T = std::make_shared<raylib::Texture2D>();
+    SuperpacmanDown1_T->Load("../resources/SuperpacmanDown1.png");
+
+    //Moving up
+    auto SuperpacmanUp1_T = std::make_shared<raylib::Texture2D>();
+    SuperpacmanUp1_T->Load("../resources/SuperpacmanUp1.png");
+}
--- game-source-code/playerResources.h
+++ game-source-code/playerResources.h
@@ -9,8 +9,12 @@ class PlayerResources
 {
 public:
     PlayerResources();
-    void updateTexture(std::shared_ptr<Player>& player, std::shared_ptr<raylib::Texture2D>& player_texture);
+    void initialiseTexture();
+    void updateTexture(std::shared_ptr<Player>& player, std::shared_ptr<raylib::Texture2D>& player_texture,const bool buttoPressed);
 private:
-    std::vector<std::shared_ptr<raylib::Texture2D>>playerTextures = {};//vector to load player textures
+    std::vector<std::shared_ptr<raylib::Texture2D>>rightTexturesNM = {};//Texture for player moving to the right (NM->normal mode)
+    std::vector<std::shared_ptr<raylib::Texture2D>>leftTexturesNM = {};//Texture for player moving to the left(NM->normal mode)
+    std::vector<std::shared_ptr<raylib::Texture2D>>upTexturesNM = {};//Texture for player moving up(NM->normal mode)
+    std::vector<std::shared_ptr<raylib::Texture2D>>downTexturesNM = {};//Texture for player moving down(NM->normal mode)
 };
 #endif
