6afa8d7b954bd28e7acc3600f1068b7efa2ac630
Danielbyte <daniel.sifiso.masango@gmail.com>
2024-10-06 14:41:33 +0200

    player textures loaded into vectors

--- game-source-code/playerResources.cpp
+++ game-source-code/playerResources.cpp
@@ -17,29 +17,36 @@ void PlayerResources::initialiseTexture()
     //Left
     auto pacmanLeft1_T = std::make_shared<raylib::Texture2D>();
     pacmanLeft1_T->Load("../resources/pacmanLeft1.png");
+    leftTexturesNM.push_back(pacmanLeft1_T);
 
     //Down
     auto pacmanDown1_T = std::make_shared<raylib::Texture2D>();
     pacmanDown1_T->Load("../resources/pacmanDown1.png");
+    downTexturesNM.push_back(pacmanDown1_T);
 
     //Up
     auto pacmanUp1_T = std::make_shared<raylib::Texture2D>();
     pacmanUp1_T->Load("../resources/pacmanUp1.png");
+    upTexturesNM.push_back(pacmanUp1_T);
 
      //SUPER-PACMAN MODE
     //To the right
     auto SuperpacmanRight1_T = std::make_shared<raylib::Texture2D>();
     SuperpacmanRight1_T->Load("../resources/SuperpacmanRight1.png");
+    rightTexturesSP.push_back(SuperpacmanRight1_T);
 
     //To the left
     auto SuperpacmanLeft1_T = std::make_shared<raylib::Texture2D>();
     SuperpacmanLeft1_T->Load("../resources/SuperpacmanLeft1.png");
+    leftTexturesSP.push_back(SuperpacmanLeft1_T);
 
     //Moving down
     auto SuperpacmanDown1_T = std::make_shared<raylib::Texture2D>();
     SuperpacmanDown1_T->Load("../resources/SuperpacmanDown1.png");
+    downTexturesSP.push_back(SuperpacmanDown1_T);
 
     //Moving up
     auto SuperpacmanUp1_T = std::make_shared<raylib::Texture2D>();
     SuperpacmanUp1_T->Load("../resources/SuperpacmanUp1.png");
+    upTexturesSP.push_back(SuperpacmanUp1_T);
 }
--- game-source-code/playerResources.h
+++ game-source-code/playerResources.h
@@ -16,5 +16,10 @@ private:
     std::vector<std::shared_ptr<raylib::Texture2D>>leftTexturesNM = {};//Texture for player moving to the left(NM->normal mode)
     std::vector<std::shared_ptr<raylib::Texture2D>>upTexturesNM = {};//Texture for player moving up(NM->normal mode)
     std::vector<std::shared_ptr<raylib::Texture2D>>downTexturesNM = {};//Texture for player moving down(NM->normal mode)
+
+    std::vector<std::shared_ptr<raylib::Texture2D>>rightTexturesSP = {};//Texture for player moving to the right(SP->Super pacman)
+    std::vector<std::shared_ptr<raylib::Texture2D>>leftTexturesSP = {};//Texture for player moving to the left(SP->Super pacman)
+    std::vector<std::shared_ptr<raylib::Texture2D>>upTexturesSP = {};//Texture for player moving up(SP->Super pacman)
+    std::vector<std::shared_ptr<raylib::Texture2D>>downTexturesSP = {};//Texture for player moving down(SP->Super pacman)
 };
 #endif
